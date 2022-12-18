5ff91a793f0a76994c811da1930fc1ae5e85451b
Aurélien Bec <aurelien.bec@gmail.com>
2022-12-19 00:00:00 +0100

    2022 day 19

---
+++ 2022/19/main.go
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2022/day/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var inputs = [...]string{}
+
+func main() {
+	defer func(start time.Time) { fmt.Println("took:", time.Since(start).Round(time.Microsecond)) }(time.Now())
+
+	// init
+
+	////////////////////////////////////////
+
+	// 0
+	fmt.Println("part1:", 0)
+
+	////////////////////////////////////////
+
+	// 0
+	fmt.Println("part2:", 0)
+}
